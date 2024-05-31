4c8ad1f283263294ea5a50d3c61b6d04c8a5c5e8
Tiya Noonia <tiyanoonia2901@gmail.com>
2024-05-31 17:11:22 +0100

    this is a calculator

---
+++ main.py
@@ -0,0 +1,47 @@
+# How to write a function
+def sum(num1, num2):
+    """
+    this function is adding two numbers together
+    :param num1:
+    :param num2:
+    :return: none
+    """
+    print(num1 + num2)
+
+def minus(num1, num2):
+    """
+    this code subtracts your numbers
+    :param num1:
+    :param num2:
+    :return: none
+    """
+    print(num1 - num2)
+
+def multiply(num1, num2):
+    """
+    this code multiplies your numbers
+    :param num1:
+    :param num2:
+    :return: none
+    """
+    print(num1 * num2)
+def divide(num1, num2):
+    """
+    this code divides your numbers
+    :param num1:
+    :param num2:
+    :return: none
+    """
+    print(num1 / num2)
+
+
+
+
+
+# how to use the function
+sum(14, 2)
+sum (3, 77)
+sum (10, 234567801)
+minus(3467, 765)
+multiply(123, 124)
+divide(100, 3)
